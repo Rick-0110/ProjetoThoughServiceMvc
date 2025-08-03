23a8f25f6dda41dc39fe19be07ef01bcc1dfd6d9
Henrique <matosh1604@gmail.com>
2025-08-03 13:37:04 -0300

    Perfil com erro

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -14,7 +14,7 @@ namespace ProjetoThoughServiceMvc.Models
         public string Nome { get; set; }
         public string CpfCnpj { get; set; }
         public string Email { get; set; }
-        public string Senha { get; set; }
+      
         public bool EhAdmin { get; set; } = false;
     }
 }
--- obj/Debug/net8.0/ToughService.AssemblyInfo.cs
+++ obj/Debug/net8.0/ToughService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7415df9d583284f8fec6718efa831c74ec9a28b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d28926a8ba65756f362c8faa3695246114367deb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
