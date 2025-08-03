a7415df9d583284f8fec6718efa831c74ec9a28b
Henrique <matosh1604@gmail.com>
2025-08-03 13:23:25 -0300

    Testando Registro

--- obj/Debug/net8.0/ToughService.AssemblyInfo.cs
+++ obj/Debug/net8.0/ToughService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7f64b158bdd48fa7bd31b5af1f4bd9cee525ad2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79dad5c9b00fff70030c477d8d83451d342677c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
