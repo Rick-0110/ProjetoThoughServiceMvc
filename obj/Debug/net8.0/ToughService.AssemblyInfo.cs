dfb925b03d234f13501032668724bdfdfd891a09
Henrique <matosh1604@gmail.com>
2025-09-20 11:13:05 -0300

    Coisa simples.

--- obj/Debug/net8.0/ToughService.AssemblyInfo.cs
+++ obj/Debug/net8.0/ToughService.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+107f1d788c1ac66f45bf3d401c12a5ef108f6e39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b592083f34a81e14ccaea9f029909d4a36aa955e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
