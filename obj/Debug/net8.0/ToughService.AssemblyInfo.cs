442d8d59146331ffb54e8f61dbc08fbe4ad909a4
Henrique <matosh1604@gmail.com>
2025-09-23 15:19:35 -0300

    Mudança no front-end do perfil

--- obj/Debug/net8.0/ToughService.AssemblyInfo.cs
+++ obj/Debug/net8.0/ToughService.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfb925b03d234f13501032668724bdfdfd891a09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acd8bc5d4e18dd5bfcbf3885f125b93d0fa1cc52")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
