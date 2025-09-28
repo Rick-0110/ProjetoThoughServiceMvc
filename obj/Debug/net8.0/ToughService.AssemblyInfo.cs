fa2635954c3328e488830d00d4c92dbbbe02bbcf
Henrique <matosh1604@gmail.com>
2025-09-28 11:54:42 -0300

    Alterações no menu front

--- obj/Debug/net8.0/ToughService.AssemblyInfo.cs
+++ obj/Debug/net8.0/ToughService.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd661550806576772a5124a5c1bdb5649086625b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db1da7a55553ec72d0b18f0cda707a63e168695e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
