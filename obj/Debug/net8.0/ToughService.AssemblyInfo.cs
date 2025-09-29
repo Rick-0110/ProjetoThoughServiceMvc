f515cb6a3c890e279a4f57cdfcfc46fbda9a306f
Henrique <matosh1604@gmail.com>
2025-09-29 17:00:42 -0300

    Mudanças no front do ADM

--- obj/Debug/net8.0/ToughService.AssemblyInfo.cs
+++ obj/Debug/net8.0/ToughService.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db1da7a55553ec72d0b18f0cda707a63e168695e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa2635954c3328e488830d00d4c92dbbbe02bbcf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
