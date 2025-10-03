3f6d7fbb023a679aa507bd15d9959522712f5322
Henrique <matosh1604@gmail.com>
2025-10-03 20:03:35 -0300

    Front do carrinho minimalista e direto ao ponto

--- obj/Debug/net8.0/ToughService.AssemblyInfo.cs
+++ obj/Debug/net8.0/ToughService.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f515cb6a3c890e279a4f57cdfcfc46fbda9a306f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c53d71626344cfe44b1869bf708f14d0ccc08c70")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
