d3a99d21a2d7fb9f21fda025edff40db62c67511
Henrique <matosh1604@gmail.com>
2025-10-25 15:07:15 -0300

    Adicionado API_viaCEP

--- obj/Debug/net8.0/ToughService.AssemblyInfo.cs
+++ obj/Debug/net8.0/ToughService.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e523c37ba483314100337a125530980ab2005544")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a70a018b027b4df552f2ef0351a078b00690961")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
