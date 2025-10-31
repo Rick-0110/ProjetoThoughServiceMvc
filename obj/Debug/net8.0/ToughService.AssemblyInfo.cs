89beb3f4b7d084ff99b7195b84e938be001d3289
Henrique <matosh1604@gmail.com>
2025-10-31 16:31:34 -0300

    Alteração menu perfil

--- obj/Debug/net8.0/ToughService.AssemblyInfo.cs
+++ obj/Debug/net8.0/ToughService.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3a99d21a2d7fb9f21fda025edff40db62c67511")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b98487bd8b51129b3a519b759ad2d12de9717137")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
