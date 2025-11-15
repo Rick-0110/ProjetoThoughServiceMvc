150203323da1a8d7b7e381cc7e28fc102108efc0
Henrique <matosh1604@gmail.com>
2025-11-15 11:07:07 -0300

    Alterações

--- obj/Debug/net8.0/ToughService.AssemblyInfo.cs
+++ obj/Debug/net8.0/ToughService.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a5944b359fd64a490f407abc2e4413dacb81775")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+423fabcac22bd98d8c20d20e31a846849a2a261d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
