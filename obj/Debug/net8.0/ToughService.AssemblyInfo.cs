1cab32f02093bd378bd29cc1079514c3583a88c7
Henrique <matosh1604@gmail.com>
2025-11-18 11:09:04 -0300

    Front do checkout feito

--- obj/Debug/net8.0/ToughService.AssemblyInfo.cs
+++ obj/Debug/net8.0/ToughService.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f67ad9fb80621f0c02881def822f8c386df91a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2271a2331546923d3203bf570fa9d5afaec63098")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
