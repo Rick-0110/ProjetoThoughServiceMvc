07a15e2ca649a99c73f73fc1aa98f5b68a6a1e96
Henrique <matosh1604@gmail.com>
2025-11-18 11:28:38 -0300

    Alteração no front, coisa básica

--- obj/Debug/net8.0/ToughService.AssemblyInfo.cs
+++ obj/Debug/net8.0/ToughService.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c523ff91ef57ed1ee41ecbc72095146f9b8bb5a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b32787e4f1d1738996fa13a4fdb5fa5bd1a38a00")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToughService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
